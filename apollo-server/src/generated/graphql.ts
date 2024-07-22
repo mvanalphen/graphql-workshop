a6f6fcbe4f135f300ff3e9da58ff19fe4e070888
Mark van Alphen <markvanalphen2@gmail.com>
2024-07-22 16:13:57 +0200

    feat: implement Customer and Order types and queries

--- apollo-server/src/generated/graphql.ts
+++ apollo-server/src/generated/graphql.ts
@@ -20,6 +20,9 @@ export type Incremental<T> =
   | {
       [P in keyof T]?: P extends " $fragmentName" | "__typename" ? T[P] : never;
     };
+export type RequireFields<T, K extends keyof T> = Omit<T, K> & {
+  [P in K]-?: NonNullable<T[P]>;
+};
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: { input: string; output: string };
@@ -28,6 +31,41 @@ export type Scalars = {
   Int: { input: number; output: number };
   Float: { input: number; output: number };
   _FieldSet: { input: any; output: any };
+};
+
+export class Customer {
+  firstName?: Maybe<Scalars["String"]["output"]>;
+  id: Scalars["ID"]["output"];
+  lastName?: Maybe<Scalars["String"]["output"]>;
+  orders: Array<Order>;
+}
+
+export class Mutation {
+  placeOrder?: Maybe<Order>;
+}
+
+export type MutationPlaceOrderArgs = {
+  id: Scalars["ID"]["input"];
+};
+
+export class Order {
+  customer: Customer;
+  deliveryDate: Scalars["String"]["output"];
+  id: Scalars["ID"]["output"];
+}
+
+export class Query {
+  customer?: Maybe<Customer>;
+  order?: Maybe<Order>;
+  orders: Array<Order>;
+}
+
+export type QueryCustomerArgs = {
+  id: Scalars["ID"]["input"];
+};
+
+export type QueryOrderArgs = {
+  id: Scalars["ID"]["input"];
 };
 
 export type WithIndex<TObject> = TObject & Record<string, any>;
@@ -140,14 +178,89 @@ export type DirectiveResolverFn<
 
 /** Mapping between all available schema types and the resolvers types */
 export type ResolversTypes = ResolversObject<{
-  Boolean: ResolverTypeWrapper<Scalars["Boolean"]["output"]>;
+  Customer: ResolverTypeWrapper<Customer>;
   String: ResolverTypeWrapper<Scalars["String"]["output"]>;
+  ID: ResolverTypeWrapper<Scalars["ID"]["output"]>;
+  Mutation: ResolverTypeWrapper<{}>;
+  Order: ResolverTypeWrapper<Order>;
+  Query: ResolverTypeWrapper<{}>;
+  Boolean: ResolverTypeWrapper<Scalars["Boolean"]["output"]>;
 }>;
 
 /** Mapping between all available schema types and the resolvers parents */
 export type ResolversParentTypes = ResolversObject<{
-  Boolean: Scalars["Boolean"]["output"];
+  Customer: Customer;
   String: Scalars["String"]["output"];
+  ID: Scalars["ID"]["output"];
+  Mutation: {};
+  Order: Order;
+  Query: {};
+  Boolean: Scalars["Boolean"]["output"];
 }>;
 
-export type Resolvers<ContextType = Context> = ResolversObject<{}>;
+export type CustomerResolvers<
+  ContextType = Context,
+  ParentType extends
+    ResolversParentTypes["Customer"] = ResolversParentTypes["Customer"],
+> = ResolversObject<{
+  firstName?: Resolver<
+    Maybe<ResolversTypes["String"]>,
+    ParentType,
+    ContextType
+  >;
+  id?: Resolver<ResolversTypes["ID"], ParentType, ContextType>;
+  lastName?: Resolver<Maybe<ResolversTypes["String"]>, ParentType, ContextType>;
+  orders?: Resolver<Array<ResolversTypes["Order"]>, ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+}>;
+
+export type MutationResolvers<
+  ContextType = Context,
+  ParentType extends
+    ResolversParentTypes["Mutation"] = ResolversParentTypes["Mutation"],
+> = ResolversObject<{
+  placeOrder?: Resolver<
+    Maybe<ResolversTypes["Order"]>,
+    ParentType,
+    ContextType,
+    RequireFields<MutationPlaceOrderArgs, "id">
+  >;
+}>;
+
+export type OrderResolvers<
+  ContextType = Context,
+  ParentType extends
+    ResolversParentTypes["Order"] = ResolversParentTypes["Order"],
+> = ResolversObject<{
+  customer?: Resolver<ResolversTypes["Customer"], ParentType, ContextType>;
+  deliveryDate?: Resolver<ResolversTypes["String"], ParentType, ContextType>;
+  id?: Resolver<ResolversTypes["ID"], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+}>;
+
+export type QueryResolvers<
+  ContextType = Context,
+  ParentType extends
+    ResolversParentTypes["Query"] = ResolversParentTypes["Query"],
+> = ResolversObject<{
+  customer?: Resolver<
+    Maybe<ResolversTypes["Customer"]>,
+    ParentType,
+    ContextType,
+    RequireFields<QueryCustomerArgs, "id">
+  >;
+  order?: Resolver<
+    Maybe<ResolversTypes["Order"]>,
+    ParentType,
+    ContextType,
+    RequireFields<QueryOrderArgs, "id">
+  >;
+  orders?: Resolver<Array<ResolversTypes["Order"]>, ParentType, ContextType>;
+}>;
+
+export type Resolvers<ContextType = Context> = ResolversObject<{
+  Customer?: CustomerResolvers<ContextType>;
+  Mutation?: MutationResolvers<ContextType>;
+  Order?: OrderResolvers<ContextType>;
+  Query?: QueryResolvers<ContextType>;
+}>;
